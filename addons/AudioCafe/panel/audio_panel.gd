d012156f5f3a512f553b7f1228b0a046eaae8ce5
Cafe-GameDev <soarxz.games@gmail.com>
2025-09-17 12:55:19 -0300

    a

--- addons/AudioCafe/panel/audio_panel.gd
+++ addons/AudioCafe/panel/audio_panel.gd
@@ -16,7 +16,7 @@ extends VBoxContainer
 @onready var playlist_rich_text_label: RichTextLabel = $CollapsibleContent/TabContainer/Playlists/PlaylistRichTextLabel
 
 @onready var interactive_vbox_container: VBoxContainer = $CollapsibleContent/TabContainer/Interactive
-@onready var interactive_grid_container: GridContainer = $CollapsibleContent/TabContainer/Interactive/InteractiveGridContainer
+@onready var interactive_rich_text_label: RichTextLabel = $CollapsibleContent/TabContainer/Interactive/InteractiveRichTextLabel
 
 @onready var assets_folder_dialog: FileDialog = $CollapsibleContent/AssetsFolderDialog
 @onready var dist_folder_dialog: FileDialog = $CollapsibleContent/DistFolderDialog
@@ -317,51 +317,23 @@ func _on_save_feedback_timer_timeout():
 	save_feedback_label.visible = false
 
 func _load_interactive_streams_to_ui():
-	if not interactive_grid_container: return
-
-	for child in interactive_grid_container.get_children():
-		child.queue_free()
+	if not interactive_rich_text_label: return
 
 	var audio_manifest = load(AUDIO_MANIFEST_PATH)
 	if not audio_manifest:
 		push_error("AudioManifest não encontrado em: " + AUDIO_MANIFEST_PATH)
 		return
 
-	for key in audio_manifest.interactive.keys():
-		var interactive_name_label = Label.new()
-		interactive_name_label.text = key
-		interactive_name_label.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-		interactive_grid_container.add_child(interactive_name_label)
+	var interactive_text = ""
+	if not audio_manifest.interactive.is_empty():
+		for key in audio_manifest.interactive.keys():
+			interactive_text += "%s\n" % key
+	else:
+		interactive_text = "Nenhum stream interativo encontrado."
 
-		var button_hbox = HBoxContainer.new()
-		
-		var delete_button = Button.new()
-		delete_button.tooltip_text = "Delete"
-		delete_button.icon = ICON_X
-		delete_button.pressed.connect(Callable(self, "_on_delete_interactive_stream_pressed").bind(key, audio_manifest.interactive[key]))
-		button_hbox.add_child(delete_button)
-		
-		interactive_grid_container.add_child(button_hbox)
+	interactive_rich_text_label.bbcode_text = interactive_text
 
-func _on_delete_interactive_stream_pressed(key: String, resource_path: String):
-	var audio_manifest = load(AUDIO_MANIFEST_PATH)
-	if not audio_manifest:
-		push_error("AudioManifest não encontrado em: " + AUDIO_MANIFEST_PATH)
-		return
 
-	if audio_manifest.interactive.has(key):
-		audio_manifest.interactive.erase(key)
-		var error = ResourceSaver.save(audio_manifest, AUDIO_MANIFEST_PATH)
-		if error != OK:
-			push_error("Falha ao salvar AudioManifest após exclusão de interactive: %s" % error)
-		else:
-			print("AudioStreamInteractive '%s' removido do manifest." % key)
-			var dir_access = DirAccess.open("")
-			if dir_access and dir_access.file_exists(resource_path):
-				dir_access.remove(resource_path)
-				print("Arquivo '%s' excluído." % resource_path)
-			_load_interactive_streams_to_ui()
-			_show_save_feedback()
 
 
 func _on_add_assets_path_button_pressed() -> void:
