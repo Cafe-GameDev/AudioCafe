7f7330e15ffa5a28c3cb9865c5414b7b810efb6a
Cafe-GameDev <soarxz.games@gmail.com>
2025-09-19 15:32:02 -0300

    a

--- addons/AudioCafe/scripts/audio_manifest.gd
+++ addons/AudioCafe/scripts/audio_manifest.gd
@@ -4,5 +4,5 @@ class_name AudioManifest
 
 @export var playlists: Dictionary = {}
 @export var randomizer: Dictionary = {}
-@export var interactive: Dictionary = {}
 @export var synchronized: Dictionary = {}
+@export var interactive: Dictionary = {}
