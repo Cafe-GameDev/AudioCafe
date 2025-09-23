87d9fe3e397d9534c9f1949d87e24bcedd4e315e
Cafe-GameDev <soarxz.games@gmail.com>
2025-09-22 23:01:35 -0300

    A

--- addons/AudioCafe/scripts/editor_property.gd
+++ addons/AudioCafe/scripts/editor_property.gd
@@ -1,3 +1,19 @@
 @tool
 extends EditorProperty
 class_name PropertyPlugin
+
+var resource_position: ResourcePosition
+
+func _init():
+    resource_position = ResourcePosition.new()
+    # Aqui você pode criar controles customizados
+    var label = Label.new()
+    label.text = "Audio Position"
+    add_child(label)
+
+func update_property():
+    # Atualiza o resource no inspector
+    emit_changed(get_edited_property(), resource_position)
+
+func _set_read_only(read_only: bool):
+    set_editable(!read_only)
--- addons/AudioCafe/scripts/inspector_plugin.gd
+++ addons/AudioCafe/scripts/inspector_plugin.gd
@@ -1,3 +1,14 @@
 @tool
 extends EditorInspectorPlugin
 class_name InspectorPlugin
+
+func can_handle(object):
+    return object is AudioStreamPlayer2D or object is AudioStreamPlayer3D
+
+func parse_begin(object):
+    if not object.has_meta("resource_position"):
+        var rp = ResourcePosition.new()
+        object.set_meta("resource_position", rp)
+    var property_plugin = ResourcePositionProperty.new()
+    property_plugin.resource_position = object.get_meta("resource_position")
+    add_property_editor("resource_position", property_plugin)
