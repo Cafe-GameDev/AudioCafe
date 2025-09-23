0c27d3b4ad241fff5e6c7ba1c7c7677a86bb8227
Cafe-GameDev <soarxz.games@gmail.com>
2025-09-22 22:36:11 -0300

    j

--- addons/AudioCafe/scripts/inspector_plugin.gd
+++ addons/AudioCafe/scripts/inspector_plugin.gd
@@ -5,5 +5,15 @@ class_name InspectorPlugin
 func _can_handle(object: Object) -> bool:
 	return object is AudioStreamPlayer2D or object is AudioStreamPlayer3D
 
+func _parse_begin(object: Object):
+	var editor_property = ClassDB.instantiate("EditorPropertyResource")
+	if not editor_property:
+		push_error("Failed to instantiate EditorPropertyResource.")
+		return
+	editor_property.set_label("Spatial Audio Config")
+	editor_property.set_object_and_property(object, "spatial_config")
+	editor_property.set_base_type("ResourcePosition")
+	add_custom_property("spatial_config", editor_property)
+
 func _parse_property(object: Object, type: int, name: String, hint_type: int, hint_string: String, usage_flags: int, wide: bool) -> bool:
-	return false
+	return false # Não precisamos mais disso, pois _parse_begin adiciona a propriedade.
