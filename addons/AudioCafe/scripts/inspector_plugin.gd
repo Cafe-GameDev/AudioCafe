3e0ee613d52d966f0aed7555f83b39a2b022ac1f
Cafe-GameDev <soarxz.games@gmail.com>
2025-09-22 23:11:48 -0300

    A

--- addons/AudioCafe/scripts/inspector_plugin.gd
+++ addons/AudioCafe/scripts/inspector_plugin.gd
@@ -9,6 +9,8 @@ func parse_begin(object):
     if not object.has_meta("resource_position"):
         var rp = ResourcePosition.new()
         object.set_meta("resource_position", rp)
-    var property_plugin = ResourcePositionProperty.new()
+    
+    var property_plugin = PropertyPlugin.new()
     property_plugin.resource_position = object.get_meta("resource_position")
+    
     add_property_editor("resource_position", property_plugin)
--- addons/AudioCafe/scripts/property_plugin.gd
+++ addons/AudioCafe/scripts/property_plugin.gd
@@ -6,14 +6,15 @@ var resource_position: ResourcePosition
 
 func _init():
     resource_position = ResourcePosition.new()
-    # Aqui você pode criar controles customizados
+    # Controle customizado
     var label = Label.new()
     label.text = "Audio Position"
     add_child(label)
 
 func update_property():
-    # Atualiza o resource no inspector
     emit_changed(get_edited_property(), resource_position)
 
-func _set_read_only(read_only: bool):
-    set_editable(!read_only)
+func _set_read_only(read_only: bool) -> void:
+    for child in get_children():
+        if child is Control:
+            child.editable = not read_only
