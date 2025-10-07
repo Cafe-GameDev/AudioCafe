9987e3f35e3b66feff7efa2016a70fc80cde7d82
Cafe-GameDev <soarxz.games@gmail.com>
2025-10-06 21:17:12 -0300

    a

--- addons/AudioCafe/scripts/generate_audio_manifest.gd
+++ addons/AudioCafe/scripts/generate_audio_manifest.gd
@@ -198,7 +198,7 @@ func generate_synchronized(audio_manifest: AudioManifest, collected_streams: Dic
 		audio_manifest.synchronized[final_key] = [
 			synchronized_file_path,
 			str(sync.stream_count),
-			ResourceLoader.get_resource_uid(synchronized_file_path)
+				var synchronized_uid = _get_uid_from_tres_file(synchronized_file_path)
 		]
 
 	return [true, ""]
@@ -284,3 +284,21 @@ func _scan_directory_for_resources(current_path: String, resource_class_name: St
 		file_or_dir_name = dir.get_next()
 
 	return true
+
+func _get_uid_from_tres_file(file_path: String) -> String:
+	var file = FileAccess.open(file_path, FileAccess.READ)
+	if file == null:
+		printerr("Falha ao abrir o arquivo .tres para ler o UID: %s" % file_path)
+		return ""
+
+	while not file.eof_reached():
+		var line = file.get_line()
+					if line.contains("uid=\"uid://"):
+					var start_index = line.find("uid=\"uid://") + len("uid=\"uid://")			var end_index = line.find(""", start_index)
+			if start_index != -1 and end_index != -1:
+				var uid = line.substr(start_index, end_index - start_index)
+				file.close()
+				return uid
+	file.close()
+	printerr("UID não encontrado no arquivo .tres: %s" % file_path)
+	return ""
